d48fff24ef74e5fafcb3cfe9aaf7db1c4e4ccd60
d4ni3lb <danielberlien@gmail.com>
2016-03-30 15:39:16 -0300

    Actualizado el segundo ejercicio para trabajar con frases

--- 24-3-16/Practico2-2.py
+++ 24-3-16/Practico2-2.py
@@ -2,15 +2,20 @@
 
 def encrypt(n):
         encriptada = ""
-        palabra = raw_input("Ingrese la palabra a encriptar: ")
+        palabra = raw_input("Ingrese la frase a encriptar: ")
         palabra = palabra.lower()
         for x in range(0,len(palabra)):
                 temp= ord(palabra[x])
-                if(temp > 96 & temp < 123):
+                if(temp > 96 and temp < 123):
+                        print("1.5")
                         if(temp+n < 123):
                                 encriptada += chr(temp + n)
+                                print ("2")
                         else:
                                 encriptada += chr(temp + n - 26)
+                                print ("3")
+                else:
+                        encriptada += chr(temp)
 
                 
         print encriptada
