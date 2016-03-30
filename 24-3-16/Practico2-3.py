acb999cc238d0a50dd32036567b4201deb2bf4e3
d4ni3lb <danielberlien@gmail.com>
2016-03-30 16:20:24 -0300

    Subido codigo para cenit polar

---
+++ 24-3-16/Practico2-3.py
@@ -0,0 +1,25 @@
+
+
+def cenitpolar():
+        encriptado = []
+        palabra = raw_input("Ingrese la frase a encriptar con cenit-polar: ")
+        palabra = palabra.lower()
+        print "probando"
+        for x in range(0,len(palabra)):
+                if(palabra[x] == "c"): encriptado.append("p")
+                elif(palabra[x] == "e"): encriptado.append("o")
+                elif(palabra[x] == "n"): encriptado.append("l")
+                elif(palabra[x] == "i"): encriptado.append("a") 
+                elif(palabra[x] == "t"): encriptado.append("r")
+                elif(palabra[x] == "p"): encriptado.append("c") 
+                elif(palabra[x] == "o"): encriptado.append("e") 
+                elif(palabra[x] == "l"): encriptado.append("n") 
+                elif(palabra[x] == "a"): encriptado.append("i")
+                elif(palabra[x] == "r"): encriptado.append("t")
+                else: encriptado.append(palabra[x])
+        final = "".join(encriptado)
+        print final
+
+        
+if __name__ == "__main__":
+        cenitpolar()
