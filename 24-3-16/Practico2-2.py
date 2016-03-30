a391ab89653c715679c4ec7df891a30c6bac2adb
d4ni3lb <danielberlien@gmail.com>
2016-03-30 17:13:47 -0300

    Version final

--- 24-3-16/Practico2-2.py
+++ 24-3-16/Practico2-2.py
@@ -7,13 +7,10 @@ def encrypt(n):
         for x in range(0,len(palabra)):
                 temp= ord(palabra[x])
                 if(temp > 96 and temp < 123):
-                        print("1.5")
                         if(temp+n < 123):
                                 encriptada += chr(temp + n)
-                                print ("2")
                         else:
                                 encriptada += chr(temp + n - 26)
-                                print ("3")
                 else:
                         encriptada += chr(temp)
 
