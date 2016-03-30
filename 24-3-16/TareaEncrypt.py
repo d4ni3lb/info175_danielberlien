39ef66ee55cea1635c0f1f4a1f81b1f1b38d8c8c
d4ni3lb <danielberlien@gmail.com>
2016-03-30 17:09:41 -0300

    Agregado primer label y algo del textbox

--- 24-3-16/TareaEncrypt.py
+++ 24-3-16/TareaEncrypt.py
@@ -1,9 +1,19 @@
 import Tkinter
 
 mainWindow = Tkinter.Tk()
+label1 = Tkinter.Label(mainWindow,anchor="center",text="Ingrese frase a encriptar: ")
+label1.pack()
+
+inputText = Tkinter.Text(mainWindow)
+inputText.pack()
+
+
 
 
 
 
 
 mainWindow.mainloop()
+
+def text_input():
+    input = self.myText_Box.get("1.0",'end-1c')
