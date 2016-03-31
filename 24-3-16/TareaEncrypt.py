b82de83ce63608052e84a9e410208def762c09ae
d4ni3lb <danielberlien@gmail.com>
2016-03-30 21:08:28 -0300

    Avanzada la interfaz gráfica

--- 24-3-16/TareaEncrypt.py
+++ 24-3-16/TareaEncrypt.py
@@ -1,11 +1,33 @@
 import Tkinter
 
+#Funciones encriptador
+cenit = False
+def sel(i):
+	if i == 1:
+		cenit = False
+		print ("Cenit falso")
+	else:
+		cenit = True
+		print ("cenit verd")
+
+
+
+
+#Interfaz grafica
+
 mainWindow = Tkinter.Tk()
-label1 = Tkinter.Label(mainWindow,anchor="center",text="Ingrese frase a encriptar: ")
+label1 = Tkinter.Label(mainWindow,anchor="center", height="3",text="Ingrese frase a encriptar: ")
 label1.pack()
 
-inputText = Tkinter.Text(mainWindow)
+inputText = Tkinter.Text(mainWindow, height="6", width="70")
 inputText.pack()
+
+label2 = Tkinter.Label(mainWindow,anchor="nw",height="2",text="Seleccione el tipo de encriptacion: ")
+label2.pack()
+
+
+rButton1 = Tkinter.Radiobutton(mainWindow, text = "Cenit-Polar",command=sel(2))
+rButton1.pack()
 
 
 
